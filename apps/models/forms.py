8f61886aa73c57a361bdd1a9c1799f5142c5e704
ale <a@414c45.net>
2017-09-14 12:11:15 +0100

    fix: Fix name to MunicipalityForm in apps.models

--- apps/models/forms.py
+++ apps/models/forms.py
@@ -1,8 +1,8 @@
 # django
 from django import forms
 
-class CityForm(forms.ModelForm):
+class MunicipalityForm(forms.ModelForm):
 
      def __init__(self, *args, **kwargs):
         self.base_fields['country'].empty_label = None
-        super(CityForm, self).__init__(*args, **kwargs)
+        super(MunicipalityForm, self).__init__(*args, **kwargs)
